29e928eb5b0a24ba06a0d7f11cef0156d198f6ae
Josue Morales <dev.carlos.morales@gmail.com>
2025-03-09 18:01:51 -0500

    chore: update astro.config.mjs to set custom site domain and base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://myhabitshero.com', // your custom domain
+  base: '/', // root path
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
